6123d87cfd008b971d10fe54119909d2dc55cdde
C-linus <c.language.enotes@gmail.com>
2025-10-02 21:16:09 +0530

    Add files via upload

---
+++ Memory Mangement II/shared_ptr.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <memory> 
+#include <string>
+
+class Project{
+    
+    private:
+        std::string m_Name;
+
+    public:
+        void SetName(const std::string &name)
+        {
+            m_Name = name;
+        }
+
+        void ShowProjectDetails() const
+        {
+            std::cout<<"[Project Name]"<<m_Name << std::endl;
+        }
+
+};
+
+class Employee
+{
+    private:
+        std::shared_ptr<Project> m_pProject = nullptr;
+
+    public:
+        void SetProject(const std::shared_ptr<Project> &prj)
+        {
+            m_pProject = prj;
+        }
+
+        const std::shared_ptr<Project>& GetProject()const {
+            return m_pProject;
+        }
+};
+
+void ShowInfo(const std::shared_ptr<Employee> &emp)
+{
+    std::cout << "Employee project details:";
+    emp->GetProject()->ShowProjectDetails();
+}
+
+int main()
+{
+    // This example explains, how a single Project* is shared between multiple employee objects. Here (Project *) is not tied to any Employee (None of them owns it), so deleting it in destructor would cause other employees to have a dangling pointer.
+
+    std::shared_ptr<Project> prj{new Project{}};
+    prj->SetName("Video Decoder");
+    std::shared_ptr<Employee> e1{new Employee{}};
+    e1->SetProject(prj);
+    std::shared_ptr<Employee> e2{new Employee{}};
+    e2->SetProject(prj);
+    std::shared_ptr<Employee> e3{new Employee{}};
+    e3->SetProject(prj);
+
+    ShowInfo(e1);
+    ShowInfo(e2);
+    prj->ShowProjectDetails();
+
+
+}
